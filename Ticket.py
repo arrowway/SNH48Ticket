b6b04adf666ad6248173e9f0bffa96265cfcfed2
北斗第一星 <18792544700@163.com>
2017-11-21 19:55:17 +0800

    Update Ticket.py

--- Ticket.py
+++ Ticket.py
@@ -39,8 +39,8 @@ class ORDER(object):
         browser.quit()
 
     def ticket(self):
-    	url = 'http://shop.48.cn'
-    	res = self.req.get(url,headers = {'Cookie':self.cookies})
+        url = 'http://shop.48.cn'
+        res = self.req.get(url,headers = {'Cookie':self.cookies})
         postData = {'id': self.ticketcode, 'num': '1', 'seattype': self.seattype,'brand_id': '3', 'r': '0.3731131006391708'}  # id:门票编号，num:门票数量，seattype:门票类型,2为VIP，3为普座，4为站票，brand_id：团体编号(gnz48为3)，’r‘:随机数
         url = 'https://shop.48.cn/tickets/saleList?id={}&brand_id=3'.format(self.ticketcode)
         types = int(self.seattype) - 1
@@ -57,23 +57,23 @@ class ORDER(object):
 	        else:
 	            continue
 	
-	def supervip(self):
-		while 1:
-			head = {'Cookie':self.cookies}
-			for i in eval(get('https://shop.48.cn/pai/GetRightShowBids?id=2423',headers = head).content)['list']:
-		list_amt.append(i['bid_amt'])
-			top1 = int(sorted(list_amt)[-1])
-			if top1 > 35000:break # 设定上限值
-			if my < top1:
-				my = int(sorted(list_amt)[-1]) + 101 # 加价额度
-				print my
-			postdata = {
-						'id':'2423',
-						'amt':str(my),
-						'num':'1'
-						}
-			res = post('https://shop.48.cn/pai/ToBids',data = postdata,headers = head)
-			time.sleep(0.2)
+    def supervip(self):
+        while 1:
+                head = {'Cookie':self.cookies}
+                for i in eval(get('https://shop.48.cn/pai/GetRightShowBids?id=2423',headers = head).content)['list']:
+        			list_amt.append(i['bid_amt'])
+                top1 = int(sorted(list_amt)[-1])
+                if top1 > 35000:break # 设定上限值
+                if my < top1:
+                    my = int(sorted(list_amt)[-1]) + 101 # 加价额度
+                    print my
+                postdata = {
+                            'id':'2423',
+                            'amt':str(my),
+                            'num':'1'
+                           }
+                res = post('https://shop.48.cn/pai/ToBids',data = postdata,headers = head)
+                time.sleep(0.2)
 
 if __name__ == '__main__':
 	se = ORDER('username','password','ticketcode','seattype')  # id:门票编号，seattype:门票类型,2为VIP，3为普座，4为站票
